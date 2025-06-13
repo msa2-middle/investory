023be2269b65c2d9dab6e153de40f7befb5faeb6
minimini <zjonyv12@naver.com>
2025-06-13 17:42:00 +0900

    초기설정

---
+++ src/main/java/stock/com/project/investory/InvestoryApplication.java
@@ -0,0 +1,13 @@
+package stock.com.project.investory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stock/com/project/investory/InvestoryApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.com.project.investory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
