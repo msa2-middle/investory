0aee7855b778cdf347d5074a33fa5bdc8afd9cc0
HEEJIN LIM <xeexin0331@gmail.com>
2025-06-16 15:17:09 +0900

    refactor/패키지명 변경

--- src/main/java/com/project/stock/investory/InvestoryApplication.java
+++ src/main/java/com/project/stock/investory/InvestoryApplication.java
@@ -1,4 +1,4 @@
-package stock.com.project.investory;
+package com.project.stock.investory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
