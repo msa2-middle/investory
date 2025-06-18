0a3c267bdb1efa7a3800be7a1a6ca4536539fea7
Patrick <jjgus29@naver.com>
2025-06-18 09:28:43 +0900

    refactor: 리팩토링 RequestDto병합, Put -> Patch

--- src/main/java/com/project/stock/investory/post/dto/RequestDto.java
+++ src/main/java/com/project/stock/investory/post/dto/RequestDto.java
@@ -9,12 +9,10 @@ import lombok.*;
 @Builder
 
 /**
- * PostCreateRequest는
  * 클라이언트(프론트엔드) → 서버로 "게시글 작성" 요청 시
  * 전달되는 데이터를 담는 DTO(Data Transfer Object)
  * */
-
-public class PostUpdateRequest {
+public class RequestDto {
     private String title;
     private String content;
 }
